adbd0e947646ede60d8a548fe51852a79bbe8bc6
diop <fodesdiop@gmail.com>
2018-10-11 13:58:50 -0700

    add tower

--- SushiNeko/GameScene.swift
+++ SushiNeko/GameScene.swift
@@ -16,7 +16,7 @@ class GameScene: SKScene {
     /* Game objects */
     var sushiBasePiece: SushiPiece!
     var character: Character!
-    
+    var sushiTower: [SushiPiece] = []
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
@@ -24,6 +24,39 @@ class GameScene: SKScene {
         character = childNode(withName: "character") as? Character
         sushiBasePiece.connectChopsticks()
         
+        // Manually stack the start of the tower
+        addTowerPiece(side: .none)
+        addTowerPiece(side: .right)
+        
+    }
+    
+    func addTowerPiece(side: Side) {
+        // Add a new Sushi Piece
+        
+        // Copy original sushi piece
+        let newPiece = sushiBasePiece.copy() as! SushiPiece
+        newPiece.connectChopsticks()
+        
+        // Access las piece
+        let lastPiece = sushiTower.last
+        
+        // Add on top of last piece
+        let lastPosition = lastPiece?.position ?? sushiBasePiece.position
+        newPiece.position.x = lastPosition.x
+        newPiece.position.y = lastPosition.y + 55
+        
+        // Increment Z to make sure it's on top of last piece
+        let lastZPosition = lastPiece?.zPosition ?? sushiBasePiece.zPosition
+        newPiece.zPosition = lastZPosition + 1
+        
+        // Set Side
+        newPiece.side = side
+        
+        // Add sushi to scene
+        addChild(newPiece)
+        
+        // Add sushi piece to the sushi tower
+        sushiTower.append(newPiece)
     }
     
 }
